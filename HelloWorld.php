56793d9ee0cbacca1dad9702e0e9009a37b73d03
wanderz <andrew.kohzk@gmail.com>
2015-07-17 06:58:17 +0000

    initial commit

---
+++ HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+    $variable = 'World';
+    echo 'Hello'.$variable.(1+1);
+    
+    # Welcome to PHP! This is a warm-up for the morning
+    # Here we learn about variables and the echo command
+    
+    # Q1: There is no space between HelloWorld. Add the space between them
+    
+    # Q2: Change the "World" to your own name.    
+    # Hint: Change $variable such that the script says 'Hello <your name> 2'
+    
+    # Q3: Change 1+1 such that the script calculates the multiplication of two numbers from two variables
+    # Hint: To multiply two numbers: $a * $b
+    
+?>
